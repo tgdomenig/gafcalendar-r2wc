bcb3be0111fee5762781d7893679f17b6944a8a0
“td” <tdomenig@it-couture.ch>
2024-06-21 16:31:56 +0200

    some testing

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+// import { useState } from "react";
+
+export default function Test() {
+
+    return(
+      <div>
+        <div>This is a Test [ZERO] (11)</div>
+        <div>Just write anything here</div>
+      </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 // https://www.npmjs.com/package/react-to-webcomponent
 
-import r2wc from '@r2wc/react-to-web-component';
+// import r2wc from '@r2wc/react-to-web-component';
 // import Test from './components/Test.js';
 import GafCalendar from './calendar/GafCalendar';
+import Test from './Test';
+import React from 'react';
+import ReactDOM from "react-dom/client" // if using React 18
+import r2wc from "react-to-webcomponent"
 
-const gafCalendar = r2wc(GafCalendar, {props: {language: "en_US"}}); //, {shadow: "open"}
+/*
+const gafCalendar = r2wc(GafCalendar, React, ReactDOM, {
+  props: {language: "string"}
+});
 
 customElements.define("gafCalendar-r2wc", gafCalendar);
+*/
+
+const test = r2wc(Test, React, ReactDOM)
+
+customElements.define("test", test);
