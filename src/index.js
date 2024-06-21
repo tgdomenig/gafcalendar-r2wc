4688d5d4721957fb74555d978ac3da48e0ba8e07
“td” <tdomenig@it-couture.ch>
2024-06-21 20:21:44 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import GafCalendar from './calendar/GafCalendar';
 import r2wc from "@r2wc/react-to-web-component"
 
 const Greeting = () => {
-  return <div>Hello, World! (2)</div>
+  return <div>Hello, World! (4)</div>
 }
 
 const WebGreeting = r2wc(Greeting, {shadow: "open"})
 
 customElements.define("web-greeting", WebGreeting)
 
-const gafCalendar = r2wc(GafCalendar, {
+const gafCalendar = r2wc(GafCalendar, {shadow: "open"}, {
   props: {language: "string"}
 });
 
-customElements.define("gafCalendar-r2wc", gafCalendar);
+customElements.define("gaf-calendar-r2wc", gafCalendar);
 
