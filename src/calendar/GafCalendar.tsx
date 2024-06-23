1c0a9bef8d752c7daac2e1384e08254ff0497114
“td” <tdomenig@it-couture.ch>
2024-06-23 14:13:42 +0200

    test

--- src/calendar/GafCalendar.tsx
+++ src/calendar/GafCalendar.tsx
@@ -31,7 +31,7 @@ export default function GafCalendar({language}: {language: string}) {
 
 
   return(
-    <div>THIS IS GAF CALENDAR (3): lg={lg} locale={locale}</div>
+    <div>THIS IS GAF CALENDAR (4): lg={lg} locale={locale}</div>
   );
 }
 
