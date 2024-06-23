fa7deb9d83779efad9429afbd5dc8088e449df1a
“td” <tdomenig@it-couture.ch>
2024-06-23 14:42:51 +0200

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// https://github.com/bitovi/react-to-web-component
+
+// import r2wc from '@r2wc/react-to-web-component';
+// import Test from './components/Test.js';
+import { useState } from 'react';
+
+import r2wc from "@r2wc/react-to-web-component"
+
+const Greeting = ({name}) => {
+  const y = useState(0);
+
+  return <div>Hello, World! (3: {name}, y={y[0]})</div>
+}
+
+const WebGreeting = r2wc(Greeting, {props: {name: "string"}})
+
+customElements.define("web-greeting", WebGreeting)
