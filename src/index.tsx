1605e1c1977cc09a9169cadcda59132ab9464588
“td” <tdomenig@it-couture.ch>
2024-06-23 14:27:50 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@
 
 // import r2wc from '@r2wc/react-to-web-component';
 // import Test from './components/Test.js';
+import { useState } from 'react';
 import GafCalendar from './calendar/GafCalendar';
 
 import r2wc from "@r2wc/react-to-web-component"
 
 const Greeting = ({name}: {name: string}) => {
-  return <div>Hello, World! (1: {name})</div>
+  const y = useState(0);
+
+  return <div>Hello, World! (2: {name}, y={y[0]})</div>
 }
 
 const WebGreeting = r2wc(Greeting, {props: {name: "string"}})
