7310cb48325d6ee9b155f2450aabc31b796e673e
“td” <tdomenig@it-couture.ch>
2024-06-23 15:05:39 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@
 
 // import r2wc from '@r2wc/react-to-web-component';
 // import Test from './components/Test.js';
-import React from 'react';
-import { useState } from 'react'
+
+import React from 'react'; // DIES IST WESENTLICH; siehe https://sentry.io/answers/react-typeerror-usestate/
+// import { useState } from 'react'
 
 import r2wc from "@r2wc/react-to-web-component"
 
-const Greeting = ({name}) => {
-  const y = useState(0);
+function Greeting({name}) {
+  const [y, setY] = React.useState(0);
 
-  return <div>Hello, World! (3: {name}, y={y[0]})</div>
+  return <div>Hello, World! (3: {name}, y={y})</div>
 }
 
 const WebGreeting = r2wc(Greeting, {props: {name: "string"}})
