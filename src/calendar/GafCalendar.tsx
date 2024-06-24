f87ca15547e97d7ca0e31a1d27b55f98c15e1cae
“td” <tdomenig@it-couture.ch>
2024-06-24 10:17:06 +0200

    test

--- src/calendar/GafCalendar.tsx
+++ src/calendar/GafCalendar.tsx
@@ -9,116 +9,7 @@ import { fetchEvents } from '../data/DataSource';
 import { stageData } from '../data/StageData';
 import { SITE, WEB_COMPONENT_NAME } from '../util/Globals';
 import { fetchListData } from '../fetch/FetchData';
-import '../styles/Styles.css'
 
-export function GafCalendarTEST({language}: {language: string}) {
-
-  const lg = language as LANGUAGE;
-
-  const locale = lg.substring(0,2);
-
-  const startOfToday = new Date();
-  startOfToday.setUTCHours(0, 0, 0, 0);
-
-  const [events, setEvents] = useState<ConcertDay[]>([]);
-
-  const [currentConcertDay, setCurrentConcertDay] = useState<ConcertDay|undefined>(undefined);
-
-  const [popupVisible, setPopupVisible] = useState(false);
-
-  const [loadedTimeRange, setLoadedTimeRange] = useState<{from: Date, to: Date}|undefined>(undefined);
-
-  const loadMoreEvents = async ({loadFrom, loadTo}: {loadFrom: Date, loadTo: Date}) => {
-
-    let newEvents = [...events] as ConcertDay[];
-    if (loadedTimeRange) {
-      const {from, to} = loadedTimeRange;
-      if (from > loadFrom) {
-        const moreConcerts = await fetchAndStage({fromDate: loadFrom, toDate: from});
-        newEvents = moreConcerts.concat(newEvents);
-      }
-      else if (to && to < loadTo) {
-        const moreConcerts = await fetchAndStage({fromDate: to, toDate: loadTo});
-        newEvents = newEvents.concat(moreConcerts);
-      }
-    }
-    else {
-      // should actually not happen
-      newEvents = await fetchAndStage({fromDate: loadFrom, toDate: loadTo});
-    }
-    updateEvents(newEvents);
-  }
-
-
-  useEffect(() => {
-
-    const loadInitial = async () => {
-      const from = addMonths(lastDayOfMonth(new Date()),-1);
-      const to = addMonths(lastDayOfMonth(new Date()),2);
-      updateEvents(await fetchAndStage({fromDate: from, toDate: to}));
-    }
-
-    loadInitial();
-  }, [])
-
-  const updateEvents = (newEvents: ConcertDay[]) => {
-    setEvents(newEvents);
-    if (newEvents.length > 0) {
-      const firstDay = newEvents[0];
-      const lastDay = newEvents[newEvents.length-1];
-      console.log("firstDay", JSON.stringify(firstDay));
-      console.log("lastDay", JSON.stringify(lastDay));
-      setLoadedTimeRange({from: firstDay.date, to: lastDay.date});
-    }
-  }
-
-  const fetchAndStage = async ({fromDate, toDate}: {fromDate: Date, toDate: Date}) : Promise<ConcertDay[]> => {
-    const endpoint = SITE + `wp-json/wp/v2/itc_cpt_concert`;
-    let events = [] as FetchedEvent[];
-    /*
-      BEMERKUNG: 
-      min_concert_date und max_concert_date werden im WP-Rest-API definiert.
-      fromDate und toDate ist im Fall der Konzerte a priori komplex, weil die Konzertdaten auch als Liste oder Range spezifiziert werden können.
-      Der Einfachheit halber wird aber nur das Feld acf_date angeschaut und es wird vorausgesetzt, dass dieses das erste Datum einer allfälligen Liste bezeichnet.
-    */
-    const customArgs = [
-      {
-        label: 'min_concert_date',
-        value: format(fromDate, "yyyyMMdd")
-      },
-      {
-        label: 'max_concert_date',
-        value: format(toDate, "yyyyMMdd")
-      }
-    ];
-
-    try {
-
-      const result = await fetchListData<FetchedEvent>({endpoint, customArgs});
-      if (result.events) {
-        events = result.events;
-      }
-    }
-    catch (e) {
-      Modal.error({
-        title: "Fehler!",
-        content: "Die Daten konnten nicht geladen werden: " + e,
-        getContainer: document.getElementById(WEB_COMPONENT_NAME) || undefined // TODO !!!!!!
-      });
-
-    }
-
-    console.log("HIER HIER HIER HIER")
-    const stagedEvents = stageData(events, lg);
-
-    return stagedEvents;
-  }
-
-
-  return(
-    <div>THIS IS GAF CALENDAR (6): lg={lg} locale={locale}</div>
-  );
-}
 
 export default function GafCalendar({language}: {language: string}) {
 
@@ -265,7 +156,7 @@ export default function GafCalendar({language}: {language: string}) {
   }
 
   return (
-    <div id="gaf-calendar-web-component" className="web-component">
+    <div id="gaf-calendar-web-component" className="gaf-calendar-rwc">
 
       <h1>GAF CALENDAR:</h1>
 
